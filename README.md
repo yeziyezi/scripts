f88e31cf624073b80127624fb06cd1c6235bfc42
xhJ <jxh98@foxmail.com>
2019-04-18 17:07:33 +0800

    修改使用说明 修改源码 修复bug

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
-# scripts
+# 脚本
 一些实用小脚本的集合，不定期更新～
-### 下载telegram频道中的图片
-链接：[download-telegram-channel-pictures.py](./download-telegram-channel-pictures.py)
+## 下载Telegram频道中的图片
 
-依赖: python3 ,Telethon,如果需要通过代理连接到telegram，还需要PySocks
+### 依赖
+[python3.x](https://www.python.org/) 
+[Telethon](https://github.com/LonamiWebs/Telethon) 
+[PySocks](https://github.com/Anorov/PySocks)
 
-其他说明：[关于Telethon的文档在这里](https://telethon.readthedocs.io/en/latest/index.html)
+## 使用方法
 
-### 待更新……
+依次安装 [python3.x](https://www.python.org/) 
+[Telethon](https://github.com/LonamiWebs/Telethon) 
+[PySocks](https://github.com/Anorov/PySocks)
+
+git clone https://github.com/Jxh98/scripts
+
+python ./download-telegram-channel-pictures.py
+
+输入手机号码  例子 +86 13012341234
+输入Telegram中的验证码  例子 12345
+
+喝杯咖啡等待一下吧！
+
+提示 Done. 即为下载完成。
+
+## 其他说明：
+[关于Telethon的文档在这里](https://telethon.readthedocs.io/en/latest/index.html)
+
--- download-telegram-channel-pictures.py
+++ download-telegram-channel-pictures.py
@@ -1,43 +1,70 @@
-from telethon import TelegramClient
-#import socks # if you need connect telegram through proxy,import this.
+from telethon import TelegramClient, sync
+import socks # 如果你需要使用代理（vpn），来连接Telegram。你需要导入它。
 from telethon.tl.types import InputMessagesFilterPhotos
 
+
 # =======================================
-'''Telegram Channel Pictures Download Tool
-You can get your api_id and api_hash at https://my.telegram.org/apps
-At the first time you run the script,you should provide your phone number 
-to login in your telegram account(needed by Telethon).
+'''Telegram 频道 图片 下载 工具
+你可以从 https://my.telegram.org/apps 获得你的 api_id 和 api_hash 
+在你第一次运行这个脚本的时候,
+你需要提供你的手机号码去登录你的Telegram账号。
+(这需要使用 Telethon)
 '''
-api_id = 12345
-api_hash = "1234sdef"
-channel_name = "AChannel"
-proxy = None  # if need,replace "None" by "(socks.SOCKS5, 'host', port)".
-picture_storage_path = "/path/to/picture/storage"
+
+''' 
+api_id = 你获得的 api_id 					（数字）
+api_hash = 你获得的 api_hash 				（hash字符串）
+channel_link = 你想下载图片的频道链接		（网址字符串）
+'''
+# 例子 使用时 需根据 自己所拥有的信息进行更改
+api_id = 123456
+api_hash = "af042a39824c6"
+channel_link = "https://t.me/xx"
+
+# SSR 可以用此配置
+proxy = (socks.SOCKS5, '127.0.0.1', 1080) 
+# 如果你不需要代理就能连接Telegram，请注释上一行 放开下一行
+#proxy = None  
+
+
+# 图片的存储路径 默认为 当前目录下的Pic
+picture_storage_path = "Pic"
 # ==========================================
 
+# 获取一个 Telegram 客户端？
 client = TelegramClient('my_session',
                         api_id=api_id,
                         api_hash=api_hash,
                         proxy=proxy
                         ).start()
 
-dialogs = client.get_dialogs()
-channel = None
-for dialog in dialogs:
-    if dialog.name == channel_name:
-        channel = dialog
-        break
 
-if channel is None:
-    print("[Error]The channel ", channel_name, " is not subscribed!")
-    exit(1)
 
+# 获取你所打开的频道的数组
+#dialogs = client.get_dialogs()
+
+# 检查你所输入的 channel_name 是否正确
+#for dialog in dialogs:
+#    if dialog.name == channel_name:
+#        channel = dialog
+#        break
+
+#if channel is None:
+#    print("[Error]The channel ", channel_name, " is not subscribed!")
+#    exit(1)
+
+# 获取该频道的图片数组
 photos = client.get_messages(channel, None, filter=InputMessagesFilterPhotos)
 total = len(photos)
 index = 0
+# 循环下载
 for photo in photos:
     filename = picture_storage_path + "/" +str(photo.id) + ".jpg"
     index = index + 1
     print("downloading:", index, "/", total, " : ", filename)
     client.download_media(photo, filename)
+	
+# 在你操作完成的时候关闭链接
+client.disconnect()
+
 print("Done.")
