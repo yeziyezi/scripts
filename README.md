6345cb4036021a9b10503a4c6243610a7a40515f
escyezi@foxmail.com <escyezi@foxmail.com>
2019-04-19 23:35:24 +0800

    fix bugs & update readme

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # scripts
 一些实用小脚本的集合，不定期更新～
 ### 下载telegram频道中的图片
-链接：[download-telegram-channel-pictures.py](./download-telegram-channel-pictures.py)
+- 链接：[download-telegram-channel-pictures.py](./download-telegram-channel-pictures.py)
 
-依赖: python3 ,Telethon,如果需要通过代理连接到telegram，还需要PySocks
-
-其他说明：[关于Telethon的文档在这里](https://telethon.readthedocs.io/en/latest/index.html)
+- 环境: python3 ,
+- 依赖: Telethon,PySocks(如果需要通过代理连接到telegram的话)。
+- 使用方法: 
+1. 在[my.telegram.org/apps](https://my.telegram.org/apps)拿到你的api-id以及api-hash
+2.  将一些变量替换成你需要的值
+3.  运行
+- 注意事项：首次运行脚本需要输入tg所绑定的手机号和验证码，然后telethon才能以你的身份登录去下载图片
+- 其他说明：[关于Telethon的文档在这里](https://telethon.readthedocs.io/en/latest/index.html)
 
 ### 待更新……
--- download-telegram-channel-pictures.py
+++ download-telegram-channel-pictures.py
@@ -1,38 +1,25 @@
-from telethon import TelegramClient
-#import socks # if you need connect telegram through proxy,import this.
+from telethon import TelegramClient,sync
+import socks #如果你不需要通过代理连接Telegram，可以删掉这一行
 from telethon.tl.types import InputMessagesFilterPhotos
-
-# =======================================
-'''Telegram Channel Pictures Download Tool
-You can get your api_id and api_hash at https://my.telegram.org/apps
-At the first time you run the script,you should provide your phone number 
-to login in your telegram account(needed by Telethon).
+proxy = None
+# =============需要被替换的值=================
 '''
-api_id = 12345
-api_hash = "1234sdef"
-channel_name = "AChannel"
-proxy = None  # if need,replace "None" by "(socks.SOCKS5, 'host', port)".
-picture_storage_path = "/path/to/picture/storage"
+api_id 你的api id
+api_hash 你的api hash
+channel_link 要下载图片的频道链接
+proxy 将localhost改成代理地址,12345改成代理端口
+picture_storage_path 图片下载到的路径
+'''
+api_id = 123456
+api_hash = "23jhbfj23kbf3wkfbwk3j8w3fbesiug"
+channel_link = "https://t.me/xxxx"
+proxy =(socks.SOCKS5,"localhost",12345) #不需要代理的话删掉该行
+picture_storage_path = "picture_storage_path"
 # ==========================================
-
-client = TelegramClient('my_session',
-                        api_id=api_id,
-                        api_hash=api_hash,
-                        proxy=proxy
-                        ).start()
-
-dialogs = client.get_dialogs()
-channel = None
-for dialog in dialogs:
-    if dialog.name == channel_name:
-        channel = dialog
-        break
-
-if channel is None:
-    print("[Error]The channel ", channel_name, " is not subscribed!")
-    exit(1)
-
-photos = client.get_messages(channel, None, filter=InputMessagesFilterPhotos)
+client = TelegramClient('my_session',api_id=api_id,api_hash=api_hash,proxy=proxy).start()
+    
+photos = client.get_messages(channel_link, None, filter=InputMessagesFilterPhotos)
+    
 total = len(photos)
 index = 0
 for photo in photos:
@@ -40,4 +27,6 @@ for photo in photos:
     index = index + 1
     print("downloading:", index, "/", total, " : ", filename)
     client.download_media(photo, filename)
+    
+client.disconnect()
 print("Done.")
