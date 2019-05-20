89313017f123dde92873b334f09be7639bdac227
xh <jxh98@foxmail.com>
2019-05-21 02:09:46 +0800

    修复一些问题

--- download-telegram-channel-pictures.py
+++ download-telegram-channel-pictures.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient, sync
 from telethon.tl.types import InputMessagesFilterPhotos
-import socks # 如果你需要使用代理（vpn），来连接Telegram。你需要导入它。
-import os # 用于读取文件，实现断点续传的功能。
+import socks  # 如果你需要使用代理（vpn），来连接Telegram。你需要导入它。
+import os  # 用于读取文件，实现断点续传的功能。
 
 # =======================================
 '''Telegram 频道 图片 下载 工具
@@ -34,9 +34,9 @@ if api_hash == "af042a39824":
     exit(1)
 
 # SSR 可以用此配置
-proxy = (socks.SOCKS5, '127.0.0.1', 1080) 
+proxy = (socks.SOCKS5, '127.0.0.1', 1080)
 # 如果你不需要代理就能连接Telegram，请注释上一行 放开下一行
-#proxy = None  
+#proxy = None
 
 
 # 图片的存储路径 默认为 当前目录下的频道名称
@@ -61,36 +61,38 @@ print("初始化Telethon成功！")
 #        channel = dialog
 #        break
 
-#if channel is None:
+# if channel is None:
 #    print("[Error]The channel ", channel_name, " is not subscribed!")
 #    exit(1)
 
 # 获取该频道的图片数组
-photos = client.get_messages(channel_link, None, filter=InputMessagesFilterPhotos)
+photos = client.get_messages(
+    channel_link, None, filter=InputMessagesFilterPhotos)
 total = len(photos)
 print("获取该频道的图片数组成功！")
 
-# 获取本地以拥有的图片名称数组
-filePath = picture_storage_path
-L = os.listdir(filePath)
 F = []
-for str1 in L:
-    F.append(str1[0:-4])
-print("获取本地以拥有的图片名称数组成功！")
+# 判断本地是否有此文件夹
+if os.path.exists(picture_storage_path):
+    # 获取本地以拥有的图片名称数组
+    L = os.listdir(picture_storage_path)
+    for str1 in L:
+        F.append(str1[0:-4])
+    print("获取本地以拥有的图片名称数组成功！")
 
 # 循环下载 支持 断点续传
-sum=0
+sum = 0
 index = 0
-for photo in photos:    
+for photo in photos:
     if str(photo.id) in F:
         # print(str(photo.id) + ".jpg"+" 已下载！")
-        sum+=1
+        sum += 1
     else:
-        filename = picture_storage_path + "/" +str(photo.id) + ".jpg"
+        filename = picture_storage_path + "/" + str(photo.id) + ".jpg"
         index = index + 1
         print("正在下载:", index, "/", total, " : ", filename)
-        client.download_media(photo,filename)
-        sum+=1
+        client.download_media(photo, filename)
+        sum += 1
 
 # 无脑循环下载
 
@@ -99,10 +101,10 @@ for photo in photos:
 #     index = index + 1
 #     print("downloading:", index, "/", total, " : ", filename)
 #     client.download_media(photo, filename)
-	
+
 if sum == total:
     print("全部下载成功！")
-else :
+else:
     print("部分下载成功，请在此运行程序，下载缺失部分！")
 
 # 在你操作完成的时候关闭链接
