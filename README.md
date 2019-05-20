804335bc2de13ac3c1dacdc64f2e84ab8eb9eaa6
xh <jxh98@foxmail.com>
2019-05-21 01:49:30 +0800

    增加了断点续传的功能,修复了一些问题。

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 一些实用小脚本的集合，不定期更新～
 ## 下载Telegram频道中的图片
 
+2019年5月21日：更新了断点续传功能，再也不用担心电脑断网、断电了。
+
 ### 依赖
 `Python3.x`  
 `Telethon`  
@@ -10,34 +12,38 @@
 
 ## 使用方法
 
-依次安装 
+需要依次安装 
 `[Python3.x]`(https://www.python.org/) 
 `[Telethon]`(https://github.com/LonamiWebs/Telethon) 
 `[PySocks]`(https://github.com/Anorov/PySocks)
 <br/>
 1. 打开 `download-telegram-channel-pictures.py` 修改以下代码  
-```Python
-api_id = 123456
-api_hash = "af042a39824c6"
-channel_link = "https://t.me/xx"
+``` Python
+api_id = 123456 #api_id
+api_hash = "af042a39824c6" #api_hash
+channel_link = "https://t.me/xx" #你所想下载的频道链接 https://t.me/ 开头
 ```
 
-2. 根据需要选择修改 以下代码  
-```Python
+``` bash
+你可以从 https://my.telegram.org/apps 获得你的 api_id 和 api_hash 
+在你第一次运行这个脚本的时候,
+你需要提供你的手机号码（+86 15512348888）去登录你的Telegram账号。(因为这需要使用 Telethon)
+```
+
+2. 根据需要选择修改 以下代码  如果你在国内使用SSR 选择第一个 如果你在国外则选择第二个 
+``` Python
 proxy = (socks.SOCKS5, '127.0.0.1', 1080)
 #proxy = None
-```  
+```
 
 3. 找个位置开启命令行
 `git clone https://github.com/Jxh98/scripts`
 `python ./download-telegram-channel-pictures.py`
 
-4. 输入手机号码  `例子 +86 13012341234`
-输入Telegram中的验证码  `例子 12345`
+4. 输入手机号码  例子 `+86 15512348888`
+输入Telegram中的验证码  例子 `12345`
 
 `喝杯咖啡等待一下吧！`
-
-5. 提示 `Done.` 即为下载完成。
 
 ## 其他说明：
 [关于Telethon的文档在这里](https://telethon.readthedocs.io/en/latest/index.html)
--- download-telegram-channel-pictures.py
+++ download-telegram-channel-pictures.py
@@ -1,7 +1,7 @@
 from telethon import TelegramClient, sync
-import socks # 如果你需要使用代理（vpn），来连接Telegram。你需要导入它。
 from telethon.tl.types import InputMessagesFilterPhotos
-
+import socks # 如果你需要使用代理（vpn），来连接Telegram。你需要导入它。
+import os # 用于读取文件，实现断点续传的功能。
 
 # =======================================
 '''Telegram 频道 图片 下载 工具
@@ -14,12 +14,24 @@ from telethon.tl.types import InputMessagesFilterPhotos
 ''' 
 api_id = 你获得的 api_id 					（数字）
 api_hash = 你获得的 api_hash 				（hash字符串）
-channel_link = 你想下载图片的频道链接		（网址字符串）
+channel_link = 你想下载图片的频道链接		 （网址字符串）
 '''
 # 例子 使用时 需根据 自己所拥有的信息进行更改
 api_id = 123456
-api_hash = "af042a39824c6"
-channel_link = "https://t.me/xx"
+api_hash = "af042a39824"
+channel_link = "https://t.me/???"
+
+if channel_link[13:] == "???":
+    print("请修改频道链接（channel_link）!")
+    exit(1)
+
+if api_id == 123456:
+    print("请修改api_id 为自己的（api_id）!")
+    exit(1)
+
+if api_hash == "af042a39824":
+    print("请修改api_hash 为自己的（api_hash）!")
+    exit(1)
 
 # SSR 可以用此配置
 proxy = (socks.SOCKS5, '127.0.0.1', 1080) 
@@ -27,9 +39,10 @@ proxy = (socks.SOCKS5, '127.0.0.1', 1080)
 #proxy = None  
 
 
-# 图片的存储路径 默认为 当前目录下的Pic
-picture_storage_path = "Pic"
+# 图片的存储路径 默认为 当前目录下的频道名称
+picture_storage_path = channel_link[13:]
 # ==========================================
+
 
 # 获取一个 Telegram 客户端？
 client = TelegramClient('my_session',
@@ -37,14 +50,13 @@ client = TelegramClient('my_session',
                         api_hash=api_hash,
                         proxy=proxy
                         ).start()
-
-
+print("初始化Telethon成功！")
 
 # 获取你所打开的频道的数组
 #dialogs = client.get_dialogs()
 
 # 检查你所输入的 channel_name 是否正确
-#for dialog in dialogs:
+# for dialog in dialogs:
 #    if dialog.name == channel_name:
 #        channel = dialog
 #        break
@@ -54,17 +66,46 @@ client = TelegramClient('my_session',
 #    exit(1)
 
 # 获取该频道的图片数组
-photos = client.get_messages(channel, None, filter=InputMessagesFilterPhotos)
+photos = client.get_messages(channel_link, None, filter=InputMessagesFilterPhotos)
 total = len(photos)
+print("获取该频道的图片数组成功！")
+
+# 获取本地以拥有的图片名称数组
+filePath = picture_storage_path
+L = os.listdir(filePath)
+F = []
+for str1 in L:
+    F.append(str1[0:-4])
+print("获取本地以拥有的图片名称数组成功！")
+
+# 循环下载 支持 断点续传
+sum=0
 index = 0
-# 循环下载
-for photo in photos:
-    filename = picture_storage_path + "/" +str(photo.id) + ".jpg"
-    index = index + 1
-    print("downloading:", index, "/", total, " : ", filename)
-    client.download_media(photo, filename)
+for photo in photos:    
+    if str(photo.id) in F:
+        # print(str(photo.id) + ".jpg"+" 已下载！")
+        sum+=1
+    else:
+        filename = picture_storage_path + "/" +str(photo.id) + ".jpg"
+        index = index + 1
+        print("正在下载:", index, "/", total, " : ", filename)
+        client.download_media(photo,filename)
+        sum+=1
+
+# 无脑循环下载
+
+# for photo in photos:
+#     filename = picture_storage_path + "/" +str(photo.id) + ".jpg"
+#     index = index + 1
+#     print("downloading:", index, "/", total, " : ", filename)
+#     client.download_media(photo, filename)
 	
+if sum == total:
+    print("全部下载成功！")
+else :
+    print("部分下载成功，请在此运行程序，下载缺失部分！")
+
 # 在你操作完成的时候关闭链接
 client.disconnect()
 
-print("Done.")
+print("关闭成功！")
